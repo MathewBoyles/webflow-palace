1e7b52f2865f0e2d5b0bd6db273d9ae4d516289c
sturzael <esturz23@gmail.com>
2018-09-19 11:39:33 +1200

    env file set up

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+require('dotenv').config();
+
 const Webflow = require('webflow-api');
 
-
-const api = new Webflow({
-  token: 'api-token'
-});
-
-api.site({
-  // siteId: '580e63e98c9a982ac9b8b741'
-}).then(site => console.log(site));
+const api_key = process.env.apikey
+console.log(api_key);
+// const api = new Webflow({
+//   token: 'api-token'
+// });
+//
+// api.site({
+//   // siteId: '580e63e98c9a982ac9b8b741'
+// }).then(site => console.log(site));
