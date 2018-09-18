514ae9b622f6184d86e1061d069b95bfd3a42f25
sturzael <esturz23@gmail.com>
2018-09-19 11:44:03 +1200

    pull data

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ require('dotenv').config();
 
 const Webflow = require('webflow-api');
 
-const api_key = process.env.apikey
-console.log(api_key);
-// const api = new Webflow({
-//   token: 'api-token'
-// });
-//
+const api_key = process.env.apikey;
+let collections_id = process.env.collectionid;
+
+
+const webflow = new Webflow({
+  token: api_key
+});
+
+const items = webflow.items({
+  collectionId: collections_id
+}, {
+  limit: 1
+});
+
+items.then(i => console.log(i));
+
+
 // api.site({
 //   // siteId: '580e63e98c9a982ac9b8b741'
 // }).then(site => console.log(site));
