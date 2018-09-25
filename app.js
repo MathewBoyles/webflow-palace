1295e017254b266bf7999bd725512431079d9549
sturzael <esturz23@gmail.com>
2018-09-25 13:22:43 +1200

    pulling n pushing one entry from palace to webflow

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ let item_id = process.env.itemX;
 let newItemID, listOfItems;
 let palacePropertycode = 'RBPR000042';
 let uniquePropertyCodes = [];
+let name, propertyaddress1,propertyaddress2, propertyaddress3, propertyaddress4, propertycode,palacePropertys;
+
 
 const webflow = new Webflow({
   token: api_key
@@ -33,8 +35,16 @@ function getPalaceListings() {
     data:{}
   }).then(function(response) {
     parseString(response.data, function (err, result) {
-        console.dir(result.ArrayOfViewAllDetailedProperty.ViewAllDetailedProperty[0]);
-    });
+        palacePropertys = result.ArrayOfViewAllDetailedProperty;
+        propertyaddress1 = palacePropertys.ViewAllDetailedProperty[0].PropertyAddress1[0];
+        propertyaddress2 = palacePropertys.ViewAllDetailedProperty[0].PropertyAddress2[0];
+        propertyaddress3 = palacePropertys.ViewAllDetailedProperty[0].PropertyAddress3[0];
+        propertyaddress4 = palacePropertys.ViewAllDetailedProperty[0].PropertyAddress4[0];
+        propertycode = palacePropertys.ViewAllDetailedProperty[0].PropertyCode[0];
+        name = `${propertycode} ${propertyaddress2} ${propertyaddress3} ${propertyaddress4}`
+    })
+    console.log("creating now");
+    create();
   }).catch(function(error) {
     return;
   })
@@ -49,11 +59,12 @@ function pullData() {
   });
   items.then(function(i) {
     listOfItems = i.items;
+    console.log(listOfItems);
     for (var i = 0; i < listOfItems.length; i++) {
-      uniquePropertyCodes.push(listOfItems[i].palacePropertycode);
+      uniquePropertyCodes.push(listOfItems[i].propertycode);
     }
-    if (uniquePropertyCodes.includes(palacePropertycode)) {
-      return;
+    if (uniquePropertyCodes.includes(propertycode)) {
+      console.log("exists already");
     } else {
       create();
     }
@@ -64,13 +75,20 @@ function create() {
   const item = webflow.createItem({
     collectionId: collections_id,
     fields: {
-      'name': 'Exciting blog post title',
+      'name': name,
       'propertycode': palacePropertycode,
+      'propertyaddress1':propertyaddress1,
+      'propertyaddress2':propertyaddress2,
+      'propertyaddress3':propertyaddress4,
+      'propertyaddress4':propertyaddress4,
       '_archived': false,
       '_draft': false,
     },
   });
-  item.then(i => console.log(i));
+  item.then(function(i) {
+    console.log("created - Webflow propertyes listed below");
+    pullData();
+  })
 }
 
 //Delete - Not in use
